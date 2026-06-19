92dc862a371b894d0db822bc448d29f252ac03cc
Aishwarya <aishwaryaak697@gmail.com>
2026-06-19 05:14:11 +0000

    progress

---
+++ Brocode/Lession1.py
@@ -0,0 +1,41 @@
+#strings
+print("ALL THE BEST")
+print("its really good")
+first_name = "AISH"
+print(first_name)
+print( f"Hello {first_name}")
+food = "PIZZA"
+email = "aish12@fake.com"
+print(f"i like to eat {food}")
+print(f"youe email is : {email}")
+print(f"i like to eat {food}")
+
+#integers
+age = 15
+quantity = 1
+no_of_students = 13
+print(f"i am {age} years old!")
+print(f"you should buy {quantity} banana")
+print(f"there are {no_of_students} students in my class")
+
+#float
+gpa = 8.0
+weight = 48
+print(f"her gpa is {gpa}")
+print(f"my weight is {weight} kgs")
+
+#boolean
+is_student = "False"
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+is_online = False
+if is_online:
+    print("you are online")
+else:
+    print ("you are offline")
+
+#typecasting 
+
